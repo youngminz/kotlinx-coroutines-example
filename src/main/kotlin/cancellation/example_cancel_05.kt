c0c428fdc1fc3ba79fd8041572755e53f0a5251e
Youngmin Koo <youngminz.kr@gmail.com>
2022-06-06 20:32:42 +0900

    example_cancel_05.kt

---
+++ src/main/kotlin/cancellation/example_cancel_05.kt
@@ -0,0 +1,32 @@
+package cancellation
+
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+// 취소할 수 없는 컨텍스트에서는 delay를 주어도 예외가 발생하지 않아서 기다릴 수 있다.
+// withContext(NonCancellable) {}
+
+fun main() = runBlocking {
+    val job = launch {
+        try {
+            repeat(1000) { i ->
+                println("job: I'm sleeping $i ...")
+                delay(500L)
+            }
+        } finally {
+            withContext(NonCancellable) {
+                println("job: I'm running finally")
+                delay(1000L)
+                println("job: And I've just delayed for 1 sec because I'm non-cancellable")
+            }
+        }
+    }
+    delay(1300L)
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin()
+    println("main: Now I can quit.")
+}
